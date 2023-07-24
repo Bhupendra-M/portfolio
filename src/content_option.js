c48e84f570d41636e0b07a036a6426c6c9946979
Mohit J <mohit@testframework.io>
2023-07-24 20:17:34 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,183 @@
+const logotext = "BIO";
+const meta = {
+    title: "Bhupendra Maithele",
+    description: "I’m Bhupendra Maithele Full stack devloper,currently working in india",
+    tagline: "Bringing Your Vision to Life, From Concept to Completion."
+};
+
+const introdata = {
+    // title: "I’m Bhupendra Maithele",
+    title: "Bhupendra Maithele",
+    animated: {
+        first: "I love coding.",
+        second: "I code cool websites.",
+        third: "I develop mobile apps.",
+        // fourth: "A freelancer."
+    },
+    description: "A Software Developer is to develop high-quality software solutions that are efficient, reliable, and user-friendly. I strive to solve problems, improve processes, and enhance user experiences through the application of my technical skills and knowledge.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Abit about my self",
+    aboutme: "As a skilled and passionate Full Stack Developer based in India, I specialize in MEAN, MERN stacks & mobile applications using the powerful Ionic framework. My commitment to excellence and constant thirst for knowledge drives me to stay ahead of the curve, adopting cutting-edge technologies and innovative techniques to deliver unparalleled results. With a proven track record of creating seamless user experiences and scalable solutions, I take pride in transforming ideas into reality, leaving a trail of satisfied clients and successful projects in my wake."
+};
+const worktimeline = [{
+        jobtitle: "Web Developer (Intern)",
+        where: "Rkhchhit Software Pvt. Ltd., Satna",
+        date: "Sep 2021 - Feb 2022",
+    },
+    {
+        jobtitle: "Junior Software Developer",
+        where: "Cyber Infrastructure Software Pvt. Ltd., Indore",
+        date: "Mar 2022 - Present",
+    },
+];
+
+const skills = [
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 65,
+    },
+    {
+        name: "Angular",
+        value: 40,
+    },
+    {
+        name: "Ionic",
+        value: 50,
+    },
+    {
+        name: "Node JS",
+        value: 70,
+    },
+    {
+        name: "Express JS",
+        value: 65,
+    },
+];
+
+const educations = [
+    {
+        name: "B.Tech (CSE)",
+        university: "RGPV, Bhopal"
+    },
+    {
+        name: "10+2",
+        university: "MPBSE, Bhopal"
+    }
+]
+
+const services = [
+    {
+        title: "Web Developer",
+        description: "Experienced web developer with a passion for crafting visually appealing and user-friendly websites. Proficient in front-end technologies like HTML, CSS, and JavaScript, as well as back-end frameworks like Node.js and Express. Skilled in responsive design and cross-browser compatibility. Committed to writing clean, efficient code and continuously learning new technologies. Excited to collaborate on projects and bring innovative ideas to life.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "As a mobile application developer specializing in Ionic framework. Proficient in creating cross-platform apps for iOS and Android, leveraging Ionic's capabilities to deliver seamless user experiences. Skilled in AngularJS and TypeScript, ensuring robust and scalable applications. Passionate about staying up-to-date with the latest trends and best practices in mobile development. Whether it's building from scratch or enhancing existing apps, I take pride in delivering high-quality solutions",
+    },
+    // {
+    //     title: "Freelancer",
+    //     description: "Versatile software developer with a passion for freelancing. As a freelancer, I offer tailored solutions to meet your unique development needs. With expertise in a wide range of programming languages and technologies, I can take on diverse projects, from web and mobile app development to custom software solutions. I pride myself on delivering high-quality work, meeting deadlines, and maintaining effective communication throughout the process. Let's collaborate and bring your ideas to life. Whether it's a one-time project or ongoing support, I'm dedicated to providing exceptional results. ",
+    // },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "bmaithele@gmail.com",
+    YOUR_FONE: "(+91) 860 272 4125",
+    description: "Thank you for visiting my portfolio. If you have any inquiries, opportunities, or just want to connect, I'd love to hear from you! Feel free to reach out through any of the following methods:",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Bhupendra-M",
+    linkedin: "https://www.linkedin.com/in/bhupendra-maithele-b1690518b/",
+    // facebook: "https://m.facebook.com/bhupendra.maithele",
+    // instragram: "https://www.instagram.com/bhupendra_maithele/",
+    // twitter: "https://twitter.com/BhupendraMaith2",
+
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
